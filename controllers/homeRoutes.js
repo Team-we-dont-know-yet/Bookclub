455d89fdb6fce89b43da37c28474e4fc14624c73
Katherine <89894177+katherinearenas@users.noreply.github.com>
2024-09-05 20:04:57 -0500

    Co-authored-by: Max LC <MaxLC00@users.noreply.github.com> Co-authored-by: RickHennessey87 <RickHennessey87@users.noreply.github.com> Co-authored-by: Chad Young <Wrandy1@users.noreply.github.com>

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,13 @@
+/* eslint-disable prettier/prettier */
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+      res.render('homepage', {  
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
